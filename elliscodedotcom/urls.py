a34a920840292b64bd2ff9809fb889c33868c692
unknown <programmer08054@gmail.com>
2022-12-16 23:29:38 -0500

    added turtle time

--- elliscodedotcom/urls.py
+++ elliscodedotcom/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('foursquares', include('foursquares.urls')),
+    path('turtletime', include('turtletime.urls')),
 ]
